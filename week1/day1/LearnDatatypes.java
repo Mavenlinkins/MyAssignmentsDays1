4df3ee5174e79916a59ec04e59636a688da8fe3b
Mavenlinkins <34787829+Mavenlinkins@users.noreply.github.com>
2025-03-30 02:11:30 +0530

    Add files via upload

---
+++ week1/day1/LearnDatatypes.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		int age=128;
+		short amount=30000;
+				long mobileNumber=8978767878L;
+				float rateofInterest=7.58668877F;
+				double usd=1.234787876677474;
+				boolean test=false;
+				char logo='Test';
+				string name="testonlhyt";
+		System.out.println(age);
+		System.out.println(mobileNumber);
+		System.out.println(rateofInterest);
+		System.out.println(usd);
+		System.out.println(rateofInterest);
+		System.out.println(test);
+		System.out.println("logo");
+		System.out.println(name);
+	}
+
+}
